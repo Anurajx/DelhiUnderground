880e09fc4450db8e8adc188eef5a55fdc005255d
Anrj007 <anurajupadhyay6@gmail.com>
2025-03-17 18:02:52 +0530

    google maps integration on dmrc page

--- lib/elements/metro.dart
+++ lib/elements/metro.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
 
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
   const Page1({super.key});
 
   @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  late GoogleMapController mapController;
+  String _mapStyle = "";
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMapStyle();
+  }
+
+  Future<void> _loadMapStyle() async {
+    String style = await rootBundle.loadString('assets/map_style.json');
+    setState(() {
+      _mapStyle = style;
+    });
+
+    // Apply style when the map is ready
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller; // Apply the style here
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 255, 255, 255),
-      child: Center(
-        child: Text(
-          "Metro Page",
-          style: TextStyle(
-            color: const Color.fromARGB(255, 0, 0, 0),
-            fontSize: 45,
-            fontWeight: FontWeight.w300,
-          ),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green[700]),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Maps Sample App'), elevation: 2),
+        body: GoogleMap(
+          style: _mapStyle,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
         ),
       ),
     );
