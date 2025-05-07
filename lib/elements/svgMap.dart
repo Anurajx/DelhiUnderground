0a965db4237708bce13841fba6a0823a993594ef
Anurajx <anurajupadhyay6@gmail.com>
2025-05-07 19:56:36 +0530

    blacked out svg map

--- lib/elements/svgMap.dart
+++ lib/elements/svgMap.dart
@@ -2,28 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SvgMap extends StatelessWidget {
+class SvgMap extends StatefulWidget {
   const SvgMap({super.key});
+
+  @override
+  State<SvgMap> createState() => _SvgMapState();
+}
+
+class _SvgMapState extends State<SvgMap> {
+  final TransformationController _controller = TransformationController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initial zoom and pan (customize these)
+    _controller.value =
+        Matrix4.identity()
+          ..scale(3.0)
+          ..translate(-170.0, -300.0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 255, 255, 255),
-      ),
+      decoration: const BoxDecoration(color: Color.fromARGB(255, 24, 24, 27)),
       child: InteractiveViewer(
+        transformationController: _controller,
         maxScale: 20,
         minScale: 1,
         panEnabled: true,
-        child: AspectRatio(
-          aspectRatio: 1.317,
-          child: SvgPicture.asset(
-            "assets/Map/svgMap1.svg",
-            fit: BoxFit.contain,
-          ),
-        ),
+        child: SvgPicture.asset("assets/Map/svgMap1.svg"),
       ),
     );
   }
