eab12d06a415e597adfaac26f51ca61881f8d0f3
Anurajx <anurajupadhyay6@gmail.com>
2025-06-12 21:33:24 +0530

    Update route.dart

--- lib/elements/route.dart
+++ lib/elements/route.dart
@@ -28,13 +28,16 @@ class RouteDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      padding: EdgeInsets.fromLTRB(15, 10, 15, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           topNavBar(context),
           tripSummary(),
           Divider(thickness: 1, color: const Color.fromARGB(255, 35, 35, 35)),
+          routeCluster(),
+          routeCluster(),
+          // routeCluster(),
           //Cancel button and option selector
           //Route summary
           //Actual route
@@ -83,7 +86,7 @@ class tripSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(0, 30, 0, 20),
+      padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
       child: Text.rich(
         TextSpan(
           children: [
@@ -96,7 +99,7 @@ class tripSummary extends StatelessWidget {
               ),
             ),
             TextSpan(
-              text: "50 Mins ", //make these a variable
+              text: "50 Minutes ", //make these a variable
               style: TextStyle(
                 color: const Color.fromARGB(255, 230, 81, 0),
                 fontSize: 20,
@@ -128,7 +131,7 @@ class tripSummary extends StatelessWidget {
               ),
             ),
             TextSpan(
-              text: "Rs 50 ",
+              text: "Rs 50. ",
               style: TextStyle(
                 color: const Color.fromARGB(255, 230, 81, 0),
                 fontSize: 20,
@@ -142,12 +145,115 @@ class tripSummary extends StatelessWidget {
   }
 }
 
+routeCluster() {
+  double height =
+      250; //presetting height at one place so that text and line indicator stay at same height
+  return Container(
+    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        lineIndicator(height), //bar line indicator on left
+        Expanded(
+          child: infoIndicator(height),
+        ), //station info and other info on right
+      ],
+    ),
+  );
+}
 
-// Text(
-//         "Your trip takes 50 Mins has 2 interchanges and costs Rs 50",
-//         style: TextStyle(
-//           color: const Color.fromARGB(255, 109, 109, 109),
-//           fontSize: 20,
-//           fontWeight: FontWeight.w500,
-//         ),
-//       ),
+lineIndicator(height) {
+  return Container(
+    width: 7,
+    height: height,
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 47, 130, 255),
+      borderRadius: BorderRadius.all(Radius.circular(50)),
+    ),
+  );
+}
+
+infoIndicator(height) {
+  return Container(
+    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+    height: height,
+    //decoration: BoxDecoration(color: const Color.fromARGB(255, 93, 93, 93)),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          //heading
+          "Bhikaji Cama Place",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          "Heaing towards Majlis Park",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(height: 2),
+        Text(
+          "Every 2 min",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(height: 2),
+        Text(
+          "Slightly Crowded",
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(102, 54, 54, 54),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          width: 150,
+          height: 30,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "30 Stations",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              Spacer(),
+              Icon(CupertinoIcons.sort_down, color: Colors.grey),
+            ],
+          ),
+        ),
+        Spacer(), //SPACERRRR
+        Text(
+          //heading
+          "Rajori Garden",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    ),
+  );
+}
